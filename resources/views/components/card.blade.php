7322ee6538946a6e74f6088ae57bd9d58adecd3d
Aqila Zafira <hartaticpg@gmail.com>
2023-12-21 16:45:35 +0700

    card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,13 @@
+<!-- Card -->
+<div class="da relative flex flex-col justify-center overflow-hidden bg-gray-50">
+    <div class="absolute inset-0 bg-center dark:bg-black"></div>
+    <div class="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
+        <div class="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
+            <img src="https://assets-pergikuliner.com/6FXvWwxBzEOzCPz2-Q542lsAilQ=/fit-in/1366x768/smart/filters:no_upscale()/https://assets-pergikuliner.com/uploads/image/picture/1507729/picture-1564368071.jpg" class="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110" alt="" />
+        </div>
+        <div class="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
+            <h1 class="font-serif text-2xl font-bold text-white shadow-xl">Nama Caffe</h1>
+            <h1 class="text-sm font-light text-gray-200 shadow-xl">Alamat sadadadadaddasdaafasfsa</h1>
+        </div>
+    </div>
+</div>
